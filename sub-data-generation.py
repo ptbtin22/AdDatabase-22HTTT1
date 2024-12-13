60de7b174246e1899259f543e5503090fc506edd
MH221B <22127131@student.hcmus.edu.vn>
2024-12-13 22:09:37 +0700

    add notesnotes

--- sub-data-generation.py
+++ sub-data-generation.py
@@ -1,6 +1,8 @@
-# order
-# online_order
-# reservation_order
-# employee_leave_balance
-# employee_day_off
-# employee_branch_history
+# order - AE ĐỂ ID_CUSTOMER NULL NHA, LẤY branch_table_listt, để orderDate vào năm 2022, 2023, để totalPrice với actualPrice = 0 hết nha
+# order-food - AE viết cái procedure thêm món đã - rồi gọi nó ở đây cho nó cập nhật cái order ở trên nha
+# online_order - AE ĐỂ num-records ít thôi nha
+# reservation_order - AE ĐỂ num-records ít thôi nha
+# employee_leave_balance - mỗi thằng id_employee có 1 record thôi nha
+# employee_day_off - AE khỏi code
+# emp_branch_history
+#  -> Lưu ý: order có lưu employee á, mà order đang ở năm 2022, 2023 thì nhân viên đó phải có lịch sử branch từ năm 2022, 2023 nha
